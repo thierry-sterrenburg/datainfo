00061b6a06b162f138363a152616bc8f202b839d
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2014-05-06 15:48:55 +0200

    result3c Shunting rule

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,10 +1,6 @@
-SELECT p.name From Person p
-WHERE EXISTS(
-SELECT * FROM Writes w, Movie m
-WHERE w.pid = p.pid
-AND m.mid = w.mid
+SELECT p.name From Person p, Writes w, Movie m 
+WHERE w.pid = p.pid AND m.mid = w.mid
 AND NOT EXISTS (
 SELECT * FROM Directs d -- the movies exists and has no director
 WHERE d.mid = m.mid
-)
 );
