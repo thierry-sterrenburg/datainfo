c86ec2dd9a0d4a2a9675348efc73cb9cfcd3711b
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2014-05-06 16:39:55 +0200

    result3d toegevoegd

---
+++ session2/result3d.sql
@@ -0,0 +1,9 @@
+SELECT p.name From Person p
+WHERE EXISTS(
+SELECT * FROM Writes w
+WHERE w.pid = p.pid
+AND NOT EXISTS(
+SELECT * FROM Movie m, Directs d, Writes q
+WHERE m.mid = q.mid AND d.mid = m.mid AND q.pid = p.pid
+)
+);
