8d64a13b1e4bedc4527648407d4ddbff62d49b7a
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2014-05-06 15:25:47 +0200

    Shunting rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,6 +1,3 @@
-SELECT p.pid From Person p
-WHERE EXISTS(
-SELECT * FROM Acts a, Movie m
+SELECT p.pid From Person p, Acts a, Movie m
 WHERE a.pid = p.pid AND m.mid = a.mid
-AND m.name = 'Back to the Future'
-);
+AND m.name = 'Back to the Future';
