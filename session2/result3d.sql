53992bbc5d43464e14d4cf45ed9a98ffc53e2911
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2014-05-06 16:40:32 +0200

    Shunting rule

--- session2/result3d.sql
+++ session2/result3d.sql
@@ -1,9 +1,6 @@
-SELECT p.name From Person p
-WHERE EXISTS(
-SELECT * FROM Writes w
+SELECT p.name From Person p, Writes w
 WHERE w.pid = p.pid
 AND NOT EXISTS(
 SELECT * FROM Movie m, Directs d, Writes q
 WHERE m.mid = q.mid AND d.mid = m.mid AND q.pid = p.pid
-)
 );
